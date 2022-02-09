94423d37cc0798dceff3a625ae52ec2ecab5cfcc
AlightKnight343 <thegodofgreatness343@gmail.com>
2022-02-09 15:23:38 +0530

    import issue

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ import express from "express";
 const app = express();
 
 //route imports
-import routes from "./routes.js";
+import routes from "../routes/testRoute.js";
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//routes
+app.use("/", testRoute);
+
 
 //listen on port PORT
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-  routes(app);
 });
