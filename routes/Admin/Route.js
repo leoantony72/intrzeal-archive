2489309c3a86188dfd7e9317dca97e38a08ae2e3
leoantony72 <leoantony102@gmail.com>
2022-03-16 19:07:03 -0700

    📦Delete Job Post(Admin)/Get Users

--- routes/Admin/Route.js
+++ routes/Admin/Route.js
@@ -2,12 +2,16 @@ import express from "express";
 const router = express.Router();
 import { addCategory } from "../../controllers/Admin/addCategory.js";
 import { banUsers, unbanUsers } from "../../controllers/Admin/banUser.js";
+import { deletePost } from "../../controllers/Admin/deletePost.js";
 import { getStat } from "../../controllers/Admin/getStat.js";
-import { getUsers } from "../../controllers/Admin/getUsers.js";
+import { getUsers, getUsersbyID } from "../../controllers/Admin/getUsers.js";
 import { checkifUserexist } from "../../middleware/checkifUserexist.js";
 
 router.post("/category", addCategory);
 router.get("/users", getUsers);
+router.get("/user", checkifUserexist, getUsersbyID);
+
+router.delete("/del-post", checkifUserexist, deletePost);
 
 router.put("/ban-users", checkifUserexist, banUsers);
 router.put("/unban-users", checkifUserexist, unbanUsers);
