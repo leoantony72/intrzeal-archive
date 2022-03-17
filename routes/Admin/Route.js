21e20c88a92f6253d31ae362e69dfaf59b035467
leoantony72 <leoantony102@gmail.com>
2022-03-16 18:49:34 -0700

    🔨Ban Users

--- routes/Admin/Route.js
+++ routes/Admin/Route.js
@@ -1,11 +1,16 @@
 import express from "express";
 const router = express.Router();
 import { addCategory } from "../../controllers/Admin/addCategory.js";
+import { banUsers, unbanUsers } from "../../controllers/Admin/banUser.js";
 import { getStat } from "../../controllers/Admin/getStat.js";
 import { getUsers } from "../../controllers/Admin/getUsers.js";
+import { checkifUserexist } from "../../middleware/checkifUserexist.js";
 
 router.post("/category", addCategory);
 router.get("/users", getUsers);
+
+router.put("/ban-users", checkifUserexist, banUsers);
+router.put("/unban-users", checkifUserexist, unbanUsers);
 
 router.get("/stat", getStat);
 
